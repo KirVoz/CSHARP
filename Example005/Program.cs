18cfcc50c6770e9e5217e445e155aaede230629e
KirVoz <k.a.voz97@gmail.com>
2023-03-25 15:49:09 +0600

    ex5

---
+++ Example005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Insert name: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "masha")
+{
+    Console.WriteLine("ooops");
+}
+else
+{
+    Console.Write("Hi, ");
+    Console.WriteLine(username);
+}
